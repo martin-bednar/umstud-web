f08f34509e922085cf340ac144b3f8c8067b92eb
Martin Bednar <bednam17@fit.cvut.cz>
2023-01-12 22:41:15 +0100

    Add landing page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<style>
+:global(html, body) {
+    margin: 0;
+    padding: 0;
+}
+</style>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<a href="/home">
+    <div class="wrapper">
+        <img src="/umstud-landing.png" alt="UMSTUD" />
+    </div>
+</a>
+
+<style>
+	img {
+		max-width: 80%;
+		max-height: 90%;
+	}
+	.wrapper {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+</style>
